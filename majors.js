dced39513df3f2ef5cef004810bc1a4f14991847
ngxson <thichthat@gmail.com>
2019-04-14 21:09:05 +0200

    nui: fix

---
+++ majors.js
@@ -0,0 +1,13 @@
+var majors = [{
+    key: 'tự_nhiên',
+    name: 'Tự nhiên',
+}, {
+    key: 'xã_hội',
+    name: 'Xã hội',
+}, {
+    key: 'cao_đẳng_nghề',
+    name: 'Cao đẳng nghề',
+}, {
+    key: 'lực_lượng_vũ_trang',
+    name: 'Lực lượng vũ trang'
+}];
